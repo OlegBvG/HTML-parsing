427de34645bd268fb4b1181108b97d05a3cf397b
OlegBvG <borisov_og@mail.ru>
2019-08-01 23:22:32 +0300

    method copyFile introduced

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -14,32 +15,44 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+
         String targetDir = "d:\\CopyLenta\\";
         Document doc = null;
+
         try {
             doc = Jsoup.connect("https://lenta.ru").get();
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("Неверный адрес ресурса => Завершение работы программы.");
+            System.exit(1);
         }
+
+        if (doc == null) {
+            System.out.println("Неверный адрес ресурса1");
+            System.exit(1);
+        }
+
         String title = doc.title();
         System.out.println("Title : " + title);
 
         Elements picture = doc.select(" img[src~=(?i)\\.(png|jpe?g)]");
         Pattern pattern = Pattern.compile("[^/][a-zA-Z0-9_]+\\.(png|jpe?g)");
 
-        picture.forEach(p -> {
-            try {
-                Matcher matcher = pattern.matcher(p.attr("src"));
-                matcher.find();
+        picture.forEach(p -> copyFile(p.attr("src"), targetDir, pattern));
 
-                InputStream in = URI.create(p.attr("src")).toURL().openStream();
-                Files.copy(in, Paths.get(targetDir, matcher.group()), StandardCopyOption.REPLACE_EXISTING);
-                System.out.println("Скопирован: " + p.attr("src") + " => " + Paths.get(targetDir, matcher.group()) );
+    }
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+    private static void copyFile(String addressFile, String targetDir, Pattern pattern){
+        try {
+            Matcher matcher = pattern.matcher(addressFile);
+            matcher.find();
+//                InputStream in = URI.create(p.attr("src")).toURL().openStream();
+            InputStream in = new URL(addressFile).openStream();
+            Files.copy(in, Paths.get(targetDir, matcher.group()), StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Скопирован: " + addressFile + " => " + Paths.get(targetDir, matcher.group()));
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
